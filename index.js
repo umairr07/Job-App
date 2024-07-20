ebf50c51e14fe17014db7566d4eb0cfbca490fb0
Umair Shaikh <imumairshaikh07@gmail.com>
2024-07-20 22:57:17 +0530

    done making jobApp and performed all the crud operations

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 const app = express()
 
 mongoose
-    .connect("mongodb://localhost:27017/job_app")
+    .connect("mongodb+srv://imumairshaikh07:fd01WVMRdcaczjH5@cluster0.kwr9lep.mongodb.net/")
     .then(() => console.log("database connected successfully"))
     .catch((err) => console.log("Error connecting with db", err))
 
